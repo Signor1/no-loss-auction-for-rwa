0daff453c510276bc6ed54e440dcaca0f1027b46
Labi-joy <joyeden325@gmail.com>
2026-01-08 06:53:28 +0100

    Governance Token Impl

---
+++ contracts/src/RWAGovernanceToken.sol
@@ -0,0 +1,241 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.23;
+
+import "./lib/IERC20.sol";
+import "./lib/SafeERC20.sol";
+
+/// @title RWAGovernanceToken
+/// @notice ERC20 token with voting checkpoints and delegation.
+contract RWAGovernanceToken is IERC20 {
+    // =============================================================
+    //                           EVENTS
+    // =============================================================
+
+    event DelegateChanged(address indexed delegator, address indexed fromDelegate, address indexed toDelegate);
+    event DelegateVotesChanged(address indexed delegate, uint256 previousBalance, uint256 newBalance);
+
+    // =============================================================
+    //                          STRUCTS
+    // =============================================================
+
+    struct Checkpoint {
+        uint32 fromBlock;
+        uint224 votes;
+    }
+
+    // =============================================================
+    //                          STORAGE
+    // =============================================================
+
+    string public name;
+    string public symbol;
+    uint8 public decimals = 18;
+    uint256 public totalSupply;
+
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    // specific to voting
+    mapping(address => address) public delegates;
+    mapping(address => uint32) public numCheckpoints;
+    mapping(address => mapping(uint32 => Checkpoint)) public checkpoints;
+
+    bytes32 public constant DOMAIN_TYPEHASH = keccak256("EIP712Domain(string name,uint256 chainId,address verifyingContract)");
+    bytes32 public constant DELEGATION_TYPEHASH = keccak256("Delegation(address delegatee,uint256 nonce,uint256 expiry)");
+
+    mapping(address => uint256) public nonces;
+
+    // =============================================================
+    //                        CONSTRUCTOR
+    // =============================================================
+
+    constructor(string memory _name, string memory _symbol) {
+        name = _name;
+        symbol = _symbol;
+        _mint(msg.sender, 1_000_000 * 1e18); // Initial supply
+    }
+
+    // =============================================================
+    //                      ERC20 FUNCTIONS
+    // =============================================================
+
+    function transfer(address recipient, uint256 amount) external returns (bool) {
+        _transfer(msg.sender, recipient, amount);
+        return true;
+    }
+
+    function approve(address spender, uint256 amount) external returns (bool) {
+        allowance[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender, amount);
+        return true;
+    }
+
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool) {
+        uint256 currentAllowance = allowance[sender][msg.sender];
+        require(currentAllowance >= amount, "ERC20: transfer amount exceeds allowance");
+        allowance[sender][msg.sender] = currentAllowance - amount;
+        _transfer(sender, recipient, amount);
+        return true;
+    }
+
+    function _transfer(address sender, address recipient, uint256 amount) internal {
+        require(sender != address(0), "ERC20: transfer from the zero address");
+        require(recipient != address(0), "ERC20: transfer to the zero address");
+
+        uint256 senderBalance = balanceOf[sender];
+        require(senderBalance >= amount, "ERC20: transfer amount exceeds balance");
+        balanceOf[sender] = senderBalance - amount;
+        balanceOf[recipient] += amount;
+
+        _moveDelegates(delegates[sender], delegates[recipient], amount);
+
+        emit Transfer(sender, recipient, amount);
+    }
+
+    function _mint(address account, uint256 amount) internal {
+        require(account != address(0), "ERC20: mint to the zero address");
+
+        totalSupply += amount;
+        balanceOf[account] += amount;
+
+        _moveDelegates(address(0), delegates[account], amount);
+
+        emit Transfer(address(0), account, amount);
+    }
+
+    function _burn(address account, uint256 amount) internal {
+        require(account != address(0), "ERC20: burn from the zero address");
+
+        uint256 accountBalance = balanceOf[account];
+        require(accountBalance >= amount, "ERC20: burn amount exceeds balance");
+        balanceOf[account] = accountBalance - amount;
+        totalSupply -= amount;
+
+        _moveDelegates(delegates[account], address(0), amount);
+
+        emit Transfer(account, address(0), amount);
+    }
+
+    // =============================================================
+    //                      VOTING FUNCTIONS
+    // =============================================================
+
+    function delegate(address delegatee) external {
+        return _delegate(msg.sender, delegatee);
+    }
+
+    function delegateBySig(
+        address delegatee,
+        uint256 nonce,
+        uint256 expiry,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external {
+        require(block.timestamp <= expiry, "ERC20Votes: signature expired");
+        bytes32 domainSeparator = keccak256(
+            abi.encode(
+                DOMAIN_TYPEHASH,
+                keccak256(bytes(name)),
+                block.chainid,
+                address(this)
+            )
+        );
+        bytes32 structHash = keccak256(
+            abi.encode(DELEGATION_TYPEHASH, delegatee, nonce, expiry)
+        );
+        bytes32 digest = keccak256(
+            abi.encodePacked("\x19\x01", domainSeparator, structHash)
+        );
+        address signatory = ecrecover(digest, v, r, s);
+        require(signatory != address(0), "ERC20Votes: invalid signature");
+        require(nonce == nonces[signatory]++, "ERC20Votes: invalid nonce");
+        _delegate(signatory, delegatee);
+    }
+
+    function getVotes(address account) external view returns (uint256) {
+        uint32 nCheckpoints = numCheckpoints[account];
+        return nCheckpoints > 0 ? checkpoints[account][nCheckpoints - 1].votes : 0;
+    }
+
+    function getPastVotes(address account, uint256 blockNumber) external view returns (uint256) {
+        require(blockNumber < block.number, "ERC20Votes: block not yet mined");
+        uint32 nCheckpoints = numCheckpoints[account];
+        if (nCheckpoints == 0) {
+            return 0;
+        }
+
+        if (checkpoints[account][nCheckpoints - 1].fromBlock <= blockNumber) {
+            return checkpoints[account][nCheckpoints - 1].votes;
+        }
+
+        if (checkpoints[account][0].fromBlock > blockNumber) {
+            return 0;
+        }
+
+        uint32 lower = 0;
+        uint32 upper = nCheckpoints - 1;
+        while (upper > lower) {
+            uint32 center = upper - (upper - lower) / 2;
+            Checkpoint memory cp = checkpoints[account][center];
+            if (cp.fromBlock == blockNumber) {
+                return cp.votes;
+            } else if (cp.fromBlock < blockNumber) {
+                lower = center;
+            } else {
+                upper = center - 1;
+            }
+        }
+        return checkpoints[account][lower].votes;
+    }
+
+    function _delegate(address delegator, address delegatee) internal {
+        address currentDelegate = delegates[delegator];
+        delegates[delegator] = delegatee;
+
+        emit DelegateChanged(delegator, currentDelegate, delegatee);
+
+        _moveDelegates(currentDelegate, delegatee, balanceOf[delegator]);
+    }
+
+    function _moveDelegates(address srcRep, address dstRep, uint256 amount) internal {
+        if (srcRep != dstRep && amount > 0) {
+            if (srcRep != address(0)) {
+                uint32 srcRepNum = numCheckpoints[srcRep];
+                uint256 srcRepOld = srcRepNum > 0 ? checkpoints[srcRep][srcRepNum - 1].votes : 0;
+                uint256 srcRepNew = srcRepOld - amount;
+                _writeCheckpoint(srcRep, srcRepNum, srcRepOld, srcRepNew);
+            }
+
+            if (dstRep != address(0)) {
+                uint32 dstRepNum = numCheckpoints[dstRep];
+                uint256 dstRepOld = dstRepNum > 0 ? checkpoints[dstRep][dstRepNum - 1].votes : 0;
+                uint256 dstRepNew = dstRepOld + amount;
+                _writeCheckpoint(dstRep, dstRepNum, dstRepOld, dstRepNew);
+            }
+        }
+    }
+
+    function _writeCheckpoint(address delegatee, uint32 nCheckpoints, uint256 oldVotes, uint256 newVotes) internal {
+        uint32 blockNumber = safe32(block.number, "ERC20Votes: block number exceeds 32 bits");
+
+        if (nCheckpoints > 0 && checkpoints[delegatee][nCheckpoints - 1].fromBlock == blockNumber) {
+            checkpoints[delegatee][nCheckpoints - 1].votes = safe224(newVotes, "ERC20Votes: vote amount exceeds 224 bits");
+        } else {
+            checkpoints[delegatee][nCheckpoints] = Checkpoint(blockNumber, safe224(newVotes, "ERC20Votes: vote amount exceeds 224 bits"));
+            numCheckpoints[delegatee] = nCheckpoints + 1;
+        }
+
+        emit DelegateVotesChanged(delegatee, oldVotes, newVotes);
+    }
+
+    function safe32(uint256 n, string memory errorMessage) internal pure returns (uint32) {
+        require(n < 2**32, errorMessage);
+        return uint32(n);
+    }
+
+    function safe224(uint256 n, string memory errorMessage) internal pure returns (uint224) {
+        require(n < 2**224, errorMessage);
+        return uint224(n);
+    }
+}
